f34af9ea183c4881fdcfee414978c199587aa3af
AndrewDRichter <andrewrichter1@hotmail.com>
2024-09-06 16:46:32 -0400

    starting coding sessions

--- adorocinema.py
+++ adorocinema.py
@@ -1,5 +1,11 @@
 from scrapy import Spider
 
+# exemplo de XPath: //a[@class='meta-title-link'] - Encontra todos os elementos em que a classe seja igual a meta-title-link
+# exemplo de XPath: //a[contains(@class,'meta-title-link')] - Encontra todos os elementos em que a classe contenha meta-title-link
+
 class AdoroCinemaSpider(Spider):
     name = 'adorocinema'
     start_urls = 'https://www.adorocinema.com/filmes/melhores'
+
+    def parse(self, response):
+        pass
