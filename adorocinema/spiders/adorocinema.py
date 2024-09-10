1dfeab5f021d8fe396c00914f3bc67dfb8f21e23
Andrew Richter <andrewrichter1@hotmail.com>
2024-09-09 23:04:13 -0300

    scrapping next pages

--- adorocinema/spiders/adorocinema.py
+++ adorocinema/spiders/adorocinema.py
@@ -1,4 +1,4 @@
-from scrapy import Spider
+from scrapy import Spider, Request
 
 # exemplo de XPath: //a[@class='meta-title-link'] - Encontra todos os elementos em que a classe seja igual a meta-title-link
 # exemplo de XPath: //a[contains(@class,'meta-title-link')] - Encontra todos os elementos em que a classe contenha meta-title-link
@@ -6,6 +6,7 @@ from scrapy import Spider
 class AdoroCinemaSpider(Spider):
     name = 'adorocinema'
     start_urls = ['https://www.adorocinema.com/filmes/melhores/',]
+    page = 1
 
     def parse(self, response):
         movies = response.xpath("//div[contains(@class,'card-list')]")
@@ -15,3 +16,5 @@ class AdoroCinemaSpider(Spider):
                 "duration": movie.css("div.meta-body-info::text").get().replace("\n", ""),
                 "original_title": movie.xpath("div[1]//div[@class='meta-body-item']/span[@class='dark-grey']/text()").get(),
             }
+        self.page += 1
+        yield Request(f'https://www.adorocinema.com/filmes/melhores/?page={self.page}', self.parse)
